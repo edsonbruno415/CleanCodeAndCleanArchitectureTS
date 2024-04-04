f017c834a8aec5a8caae63ef182654c1e794db3f
Edson Bruno <edsonbruno415@gmail.com>
2024-04-03 23:40:29 -0300

    feat: reafactor class Database

--- src/api.ts
+++ src/api.ts
@@ -5,19 +5,44 @@ import { validate } from "./validateCpf";
 const app = express();
 app.use(express.json());
 
-const isValidName = name => name.match(/[a-zA-Z] [a-zA-Z]+/);
+const isValidName = name => name && name.match(/[a-zA-Z] [a-zA-Z]+/);
 
-const isValidEmail = email => email.match(/^(.+)@(.+)$/);
+const isValidEmail = email => email && email.match(/^(.+)@(.+)$/);
 
-const isValidCarPlate = carPlate => carPlate.match(/[A-Z]{3}[0-9]{4}/);
+const isValidCarPlate = carPlate => carPlate && carPlate.match(/[A-Z]{3}[0-9]{4}/);
+
+class Database {
+  private INSERT_COMMAND = "insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)";
+  private FIND_BY_EMAIL = "select * from cccat16.account where email = $1";
+
+  constructor(readonly connectionString) {
+    this.connectionString = pgp()(connectionString);
+  }
+
+  async insertOne({ id, name, email, cpf, carPlate, isPassenger, isDriver }) {
+    return await this.connectionString.query(this.INSERT_COMMAND, [id, name, email, cpf, carPlate, isPassenger, isDriver]);
+  }
+
+  async findByEmail({ email }) {
+    return await this.connectionString.query(this.FIND_BY_EMAIL, [email]);
+  }
+
+  async close(){
+    return await this.connectionString.$pool.end();
+  }
+}
+
+const POSTGRES_CONNECTION = "postgres://postgres:123456@localhost:5432/app";
 
 app.post("/signup", async function (req, res) {
   let result;
-  const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+  const { name, email, cpf, carPlate, isPassenger, isDriver } = req.body;
+
+  const database = new Database(POSTGRES_CONNECTION);
   try {
     const id = crypto.randomUUID();
 
-    const [acc] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
+    const [acc] = await database.findByEmail({ email });
 
     if (acc) throw new Error('Account already exists');
     if (!isValidName(req.body.name)) throw new Error('Invalid name');
@@ -26,7 +51,7 @@ app.post("/signup", async function (req, res) {
 
     if (req.body.isDriver) {
       if (!isValidCarPlate(req.body.carPlate)) throw new Error('Invalid CarPlate');
-      await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+      await database.insertOne({ id, name, email, cpf, carPlate, isPassenger: !!isPassenger, isDriver: !!isDriver });
 
       const obj = {
         accountId: id
@@ -35,7 +60,7 @@ app.post("/signup", async function (req, res) {
 
     }
     if (req.body.isPassenger) {
-      await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+      await database.insertOne({ id, name, email, cpf, carPlate, isPassenger: !!isPassenger, isDriver: !!isDriver });
 
       const obj = {
         accountId: id
@@ -49,7 +74,7 @@ app.post("/signup", async function (req, res) {
     res.send(error.message);
   }
   finally {
-    await connection.$pool.end();
+    await database.close();
   }
 });
 
