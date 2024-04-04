9b20b9bf5c4bd29dd236cc9cb14b3f2baca30c6c
Edson Bruno <edsonbruno415@gmail.com>
2024-04-03 21:38:36 -0300

    feat: refactor email, name and cpf validation

--- src/api.ts
+++ src/api.ts
@@ -5,65 +5,56 @@ import { validate } from "./validateCpf";
 const app = express();
 app.use(express.json());
 
+const isValidName = name => name.match(/[a-zA-Z] [a-zA-Z]+/);
+
+const isValidEmail = email => email.match(/^(.+)@(.+)$/);
+
 app.post("/signup", async function (req, res) {
-	let result;
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		const id = crypto.randomUUID();
+  let result;
+  const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+  try {
+    const id = crypto.randomUUID();
 
-		const [acc] = await connection.query("select * from cccat15.account where email = $1", [req.body.email]);
-		if (!acc) {
+    const [acc] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
+    console.log('ACC', acc);
+    if (acc) throw new Error('Account already exists');
+    if (!isValidName(req.body.name)) throw new Error('Invalid name');
+    if (!isValidEmail(req.body.email)) throw new Error('Invalid Email');
+    if (!validate(req.body.cpf)) throw new Error('Invalid CPF');
 
-			if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (req.body.email.match(/^(.+)@(.+)$/)) {
+    if (req.body.isDriver) {
+      if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
+        await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
 
-					if (validate(req.body.cpf)) {
-						if (req.body.isDriver) {
-							if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-								
-								const obj = {
-									accountId: id
-								};
-								result = obj;
-							} else {
-								// invalid car plate
-								result = -5;
-							}
-						} else {
-							await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+        const obj = {
+          accountId: id
+        };
+        result = obj;
+      } else {
+        // invalid car plate
+        result = -5;
+      }
+    } else {
+      await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
 
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						}
-					} else {
-						// invalid cpf
-						result = -1;
-					}
-				} else {
-					// invalid email
-					result = -2;
-				}
-
-			} else {
-				// invalid name
-				result = -3;
-			}
-
-		} else {
-			// already exists
-			result = -4;
-		}
-		if (typeof result === "number") {
-			res.status(422).send(result + "");
-		} else {
-			res.json(result);
-		}
-	} finally {
-		await connection.$pool.end();
-	}
+      const obj = {
+        accountId: id
+      };
+      result = obj;
+    }
+    if (typeof result === "number") {
+      console.log('ERROR ' + result)
+      res.status(422).send(result + "");
+    }
+    res.json(result);
+  }
+  catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+  finally {
+    await connection.$pool.end();
+  }
 });
 
-app.listen(3000);
+app.listen(3005);
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-axios.defaults.validateStatus = function () {
+/* axios.defaults.validateStatus = function () {
 	return true;
-}
+} */
 
 test("Deve criar uma conta para o passageiro", async function () {
 	const input = {
@@ -11,7 +11,93 @@ test("Deve criar uma conta para o passageiro", async function () {
 		cpf: "87748248800",
 		isPassenger: true
 	};
-	const output = await axios.post("http://localhost:3000/signup", input);
+  const expected = {
+    output: {
+      status: 200,
+      data: {
+        accountTd: '7892b2aa-7538-467a-a0f1-6e96a2ca9e8c',
+      },
+    },
+  };
+
+	const output = await axios.post("http://localhost:3005/signup", input);
+  expect(output.status).toBe(expected.output.status);
 	console.log('STATUS', output.status);
   console.log('DATA:', output.data);
+});
+
+test("should be return an error when email already exists", async function () {
+	const input = {
+		name: "John Doe",
+    email: `jaojao001@gmail.com`,
+		cpf: "87748248800",
+		isPassenger: true
+	};
+  const expected = {
+    output: {
+      status: 422,
+      data: 'Account already exists',
+    },
+  };
+
+	const output = await axios.post("http://localhost:3005/signup", input);
+  // expect(output.status).toBe(expected.output.status);
+  expect(output.data).toBe(expected.output.data);
+});
+
+test("should be return an error when send wrong name", async function () {
+	const input = {
+		name: "12563@",
+    email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "87748248800",
+		isPassenger: true
+	};
+  const expected = {
+    output: {
+      status: 422,
+      data: 'Invalid name',
+    },
+  };
+
+	const output = await axios.post("http://localhost:3005/signup", input);
+  // expect(output.status).toBe(expected.output.status);
+  expect(output.data).toBe(expected.output.data);
+});
+
+test("should be return an error when send wrong email", async function () {
+	const input = {
+		name: "John Doe",
+    email: `AAAAAAAAA`,
+		cpf: "87748248800",
+		isPassenger: true
+	};
+  const expected = {
+    output: {
+      status: 422,
+      data: 'Invalid Email',
+    },
+  };
+
+	const output = await axios.post("http://localhost:3005/signup", input);
+  // expect(output.status).toBe(expected.output.status);
+  expect(output.data).toBe(expected.output.data);
+});
+
+test("should be return an error with wrong cpf", async function () {
+	const input = {
+		name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "111",
+		isPassenger: true
+	};
+  const expected = {
+    output: {
+      status: 422,
+      data: 'Invalid CPF',
+    },
+  };
+
+	const output = await axios.post("http://localhost:3005/signup", input);
+  // expect(output.status).toBe(expected.output.status);
+  expect(output.data).toBe(expected.output.data);
 });
